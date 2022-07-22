1c4b6ff19de0c6daae8382bda717c5e7f7c7c5e3
Bahaeddine Boulaabi <bahaeddine.boulaabi@bti-advisory.com>
2022-07-22 13:52:50 +0200

    Initial Commit

---
+++ Bitrise demo/Bitrise_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitrise_demoApp.swift
+//  Bitrise demo
+//
+//  Created by Bahaeddine Boulaabi on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bitrise_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
